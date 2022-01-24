9b96c2be04f74c796076f00e59bf37086ef9c5ae
chandrashekar1rn18ec406 <chandrashekar1rn18ec406@gmail.com>
2022-01-24 11:05:51 +0530

    file added

--- Jan-23_Practice_Task/value-key.js
+++ Jan-23_Practice_Task/value-key.js
@@ -1,21 +1,3 @@
-// var getKey = function( obj, value ) {
-// 	var inverse = _.invert( obj );
-// 	return _.get( inverse, value, false );
-// };
-
-// var objects ={
-//     name : 'chandrashekar',
-//     email : 'chandrashekar853@gmail.com',
-//     mobile : 9535262717
-// };
-
-
-// // var key = objects.getKey(9535262717);
-// // console.log(key); 
-
-// // keyByValue(9535262717);
-
-// console.log( 'The key for "Massachusetts" is "' + getKey( objects, 9535262717 ) + '"' );
 
 function getKeyByValue(object, value) {
     return Object.keys(object).find(key => 
