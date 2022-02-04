3991b0aa6ea3383e914ced5a274d3b6f3eeaa813
chandrashekar1rn18ec406 <chandrashekar1rn18ec406@gmail.com>
2022-02-04 12:52:57 +0530

    added

--- Jan-24/program-2.js
+++ Jan-24/program-2.js
@@ -1,23 +1,4 @@
 
-// function evenNumber(num){
-//     console.log(num + " is even!");
-// }
-// function oddNumber(num){
-//     console.log(num + " is odd!");   
-// }
-// function number(num, oddCallback, evenCallback){
-//     if(num % 2 == 0){
-//         evenCallback(num);
-//     } else {
-//         oddCallback(num);
-//     }
-// }
-// var testNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// testNumbers.forEach(function(num){
-//     number(num, oddNumber, evenNumber)
-// });
-
 function isOdd(number) {
     return number % 2 != 0;
   }
