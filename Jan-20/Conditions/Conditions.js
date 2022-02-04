c33dcd0040dd7f21448cc2b74554aa385e5d769d
chandrashekar1rn18ec406 <chandrashekar1rn18ec406@gmail.com>
2022-02-04 12:45:21 +0530

    added

--- Jan-20/Conditions/Conditions.js
+++ Jan-20/Conditions/Conditions.js
@@ -1,14 +1,20 @@
-var age = 23;
+function bar(age){
+    switch(true){
+        case age > 21:
+            console.log("Come on in !");
+            break;
+        case age > 18 && age <21:
+            console.log("Come on in(but no drinking) !");
+            break;
+        case age < 18:
+            console.log("You're too young to be in here !");
+            break;
+        case age >75 :
+            console.log('Are you sure you want to be here ?');
+            break;
+        default:
+            console.log("Invalid Age");
+    }
+}
 
-if (age >21){
-    console.log( "Come on in!");
-}else if(age >= 18 && age<=21){
-    console.log("Come on in (but no drinking)!");
-
-}
-else if(age<18){
-    console.log("You're too young to be in here!");
-}
-else if (age> 75) { 
-    console.log("Are you sure you want to be here?");
-}
+bar(84)
