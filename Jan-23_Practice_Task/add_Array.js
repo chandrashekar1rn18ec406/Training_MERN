eff0cc685f02e33ea8a6217b9af92cfdabe10a07
chandrashekar1rn18ec406 <chandrashekar1rn18ec406@gmail.com>
2022-02-04 12:56:57 +0530

    addded

--- Jan-23_Practice_Task/add_Array.js
+++ Jan-23_Practice_Task/add_Array.js
@@ -6,10 +6,3 @@ const C = [...A,  ...B];
 console.log(C);
 
 
-
-//Add two array using spread and push operator
-
-const array1 = [2, 5, 7, 9];
-const array2 = [20, 50, 70, 90];
-array1.push(...array2);
-console.log(array1);
