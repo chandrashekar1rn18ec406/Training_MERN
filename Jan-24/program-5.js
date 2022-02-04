b8910ac95153f956ee0dd6b67d34746120bd59d7
chandrashekar1rn18ec406 <chandrashekar1rn18ec406@gmail.com>
2022-02-04 13:07:16 +0530

    add

--- Jan-24/program-5.js
+++ Jan-24/program-5.js
@@ -2,8 +2,8 @@ function charDuplicate(str, noOfDuplicates) {
     var newStr = "";
   
     for (let i = 0; i < str.length; i++) {
-    //   newStr += str.charAt(i).repeat(parseInt(noOfDuplicates));
-      console.log(str[i].repeat(parseInt(noOfDuplicates)));
+      newStr += str.charAt(i).repeat(parseInt(noOfDuplicates));
+      // console.log(str[i].repeat(parseInt(noOfDuplicates)));
     }
   
     return newStr;
