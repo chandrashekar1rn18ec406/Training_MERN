e372b375cc66a87746ec8a40a882f7a0dd5972ac
chandrashekar1rn18ec406 <chandrashekar1rn18ec406@gmail.com>
2022-02-04 12:58:59 +0530

    addded

--- Jan-24/program-2.js
+++ Jan-24/program-2.js
@@ -2,11 +2,12 @@
 function isOdd(number) {
     return number % 2 != 0;
   }
-  function isEven(number) {
+
+function isEven(number) {
     return number % 2 == 0;
   }
   
-  function filter(numbers, fn) {
+function filter(numbers, fn) {
     let results = [];
     for (const number of numbers) {
       if (fn(number)) {
@@ -15,7 +16,9 @@ function isOdd(number) {
     }
     return results;
   }
-  let numbers = [1, 2, 4, 7, 3, 5, 6];
+
   
-  console.log(filter(numbers, isOdd));
-  console.log(filter(numbers, isEven));
+let numbers = [1, 2, 4, 7, 3, 5, 6];
+  
+console.log(filter(numbers, isOdd));
+console.log(filter(numbers, isEven));
